9f75f0a9e417af655c80ce38986295047528ef23
babucarr32 <baboucarry@gmail.com>
2023-06-16 10:28:26 +0000

    About to push

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div className="flex rounded-3xl bg-white overflow-hidden font-hanken">
-        <div className="p-10 text-center bg-gradient-to-b from-indigo-900 to-indigo-950">
+        <div className="p-10 text-center bg-gradient-to-b from-indigo-900 to-indigo-950 w-96">
           <p>Your Result</p>
           <div className="w-60 flex justify-center items-center mx-auto m-10 h-60 rounded-full bg-gradient-to-b from-indigo-950 to-indigo-900">
             <p>
@@ -23,15 +23,13 @@ function App() {
           <p>
             {" "}
             You scored higher than 65% of the people who have taken these tests.
-            Summary Reaction 80 / 100 Memory 92 / 100 Verbal 61 / 100 Visual 72
-            / 100 Continue
           </p>
         </div>
-        <div className="text-black p-10 pr-0 flex items-center text-left w-full">
+        <div className="text-black p-10 flex items-center text-left w-96">
           <div className="w-full">
             <p className="mb-5 font-bold text-xl">Summary</p>
             <ul className="mr-0 w-full">
-              <li className="bg-red-100 p-3 rounded-lg w-full flex items-center gap-80 mb-3">
+              <li className="bg-red-100 p-3 rounded-lg flex items-center justify-between mb-3">
                 <div className="flex gap-3">
                   <img src="/assets/images/icon-reaction.svg" alt="" />
                   <p className="text-red-600">Reaction</p>
@@ -40,7 +38,7 @@ function App() {
                   <span className="font-bold">72</span> /100
                 </p>
               </li>
-              <li className="bg-yello-100 p-3 rounded-lg w-full flex items-center gap-80 mb-3">
+              <li className="bg-yello-100 p-3 rounded-lg w-full flex items-center justify-between mb-3">
                 <div className="flex gap-3">
                   <img src="/assets/images/icon-memory.svg" alt="" />
                   <p className="text-yellow-600">Memory</p>
@@ -49,7 +47,7 @@ function App() {
                   <span className="font-bold">72</span> /100
                 </p>
               </li>
-              <li className="bg-green-100 p-3 rounded-lg w-full flex items-center gap-80 mb-3">
+              <li className="bg-green-100 p-3 rounded-lg w-full flex items-center justify-between mb-3">
                 <div className="flex gap-3">
                   <img src="/assets/images/icon-verbal.svg" alt="" />
                   <p className="text-green-600">verbal</p>
@@ -58,7 +56,7 @@ function App() {
                   <span className="font-bold">72</span> /100
                 </p>
               </li>
-              <li className="bg-indigo-100 p-3 rounded-lg w-full flex items-center gap-80 mb-3">
+              <li className="bg-indigo-100 p-3 rounded-lg w-full flex items-center justify-between mb-3">
                 <div className="flex gap-3">
                   <img src="/assets/images/icon-visual.svg" alt="" />
                   <p className="text-indigo-900">Visual</p>
@@ -67,10 +65,12 @@ function App() {
                   <span className="font-bold">72</span> /100
                 </p>
               </li>
+              <li className="text-white bg-slate-800 p-3 rounded-full w-full flex items-center gap-80 mb-3 justify-center font-extrabold">
+                <p>Continue</p>
+              </li>
             </ul>
           </div>
         </div>
-        Challenge by{" "}
       </div>
       <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
         Frontend Mentor
