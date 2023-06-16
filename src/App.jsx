abd2517b53ca8aba2be29f7bad56ffbe44054931
babucarr32 <baboucarry@gmail.com>
2023-06-16 10:55:52 +0000

    Adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className=" block rounded-none sm:flex rounded-3xl bg-white overflow-hidden font-hanken">
-        <div className="p-10 rounded-br-[35px] rounded-bl-[35px] text-center bg-gradient-to-b from-indigo-900 to-indigo-950 w-96">
+      <div className="block rounded-none sm:rounded-3xl sm:flex bg-white overflow-hidden font-hanken">
+        <div className="p-10 rounded-br-[35px] rounded-bl-[35px] sm:rounded-none text-center bg-gradient-to-b from-indigo-900 to-indigo-950 w-96">
           <p className="font-extrabold text-xl">Your Result</p>
           <div className="w-60 flex justify-center items-center mx-auto m-5 h-60 rounded-full bg-gradient-to-b from-indigo-950 to-indigo-900">
             <p>
@@ -22,41 +18,42 @@ function App() {
           </div>
           <p>
             {" "}
-            You scored higher than 65% of the people who have taken these tests.
+            You scored higher than 65% of <br /> the people who have taken{" "}
+            <br /> these tests.
           </p>
         </div>
         <div className="text-black p-10 flex items-center text-left w-96">
           <div className="w-full">
             <p className="mb-5 font-bold text-xl">Summary</p>
             <ul className="mr-0 w-full">
-              <li className="bg-red-100 p-3 rounded-lg flex items-center justify-between mb-3">
+              <li className="bg-red-100 p-4 rounded-lg flex items-center justify-between mb-4">
                 <div className="flex gap-3">
                   <img src="/assets/images/icon-reaction.svg" alt="" />
                   <p className="text-red-600">Reaction</p>
                 </div>
-                <p>
-                  <span className="font-bold">72</span> /100
+                <p className="text text-slate-600">
+                  <span className="font-bold text-black">80</span> /100
                 </p>
               </li>
-              <li className="bg-yello-100 p-3 rounded-lg w-full flex items-center justify-between mb-3">
+              <li className="bg-yello-100 p-4 rounded-lg w-full flex items-center justify-between mb-4">
                 <div className="flex gap-3">
                   <img src="/assets/images/icon-memory.svg" alt="" />
                   <p className="text-yellow-600">Memory</p>
                 </div>
-                <p>
-                  <span className="font-bold">72</span> /100
+                <p className="text text-slate-600">
+                  <span className="font-bold text-black">92</span> /100
                 </p>
               </li>
-              <li className="bg-green-100 p-3 rounded-lg w-full flex items-center justify-between mb-3">
+              <li className="bg-green-100 p-4 rounded-lg w-full flex items-center justify-between mb-4">
                 <div className="flex gap-3">
                   <img src="/assets/images/icon-verbal.svg" alt="" />
                   <p className="text-green-600">verbal</p>
                 </div>
-                <p>
-                  <span className="font-bold">72</span> /100
+                <p className="text text-slate-600">
+                  <span className="font-bold text-black">61</span> /100
                 </p>
               </li>
-              <li className="bg-indigo-100 p-3 rounded-lg w-full flex items-center justify-between mb-3">
+              <li className="bg-indigo-100 p-4 rounded-lg w-full flex items-center justify-between mb-4">
                 <div className="flex gap-3">
                   <img src="/assets/images/icon-visual.svg" alt="" />
                   <p className="text-indigo-900">Visual</p>
